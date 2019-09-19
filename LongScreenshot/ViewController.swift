1529c9f073dcf9b9f81c757fbd2810cf039938c0
Guang Lei <575403441@qq.com>
2019-09-19 12:28:36 +0800

    Initial Commit

---
+++ LongScreenshot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LongScreenshot
+//
+//  Created by Guang Lei on 2019/9/19.
+//  Copyright © 2019 Guang Lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
